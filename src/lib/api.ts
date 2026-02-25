b5a7ef50193365ab21c61bdac5b03a9852746409
amritansh333 <153156600+amritansh333@users.noreply.github.com>
2026-02-25 16:35:58 +0530

    fixed baseurl

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const api = axios.create({
-  baseURL: '${BASE_URL}/api',
+  baseURL: `${BASE_URL}/api`,
   headers: {
     "Cache-Control": "no-cache",
     Pragma: "no-cache",
